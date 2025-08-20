933ac5198895de81d5ddfdc32eff97a80d659d12
Shubham Shah <shahshubham1888@gmail.com>
2025-08-20 11:19:21 +0545

    Fix: Note component is updating while toggling delete or favourite.

--- app/admin/note/InputPage.js
+++ app/admin/note/InputPage.js
@@ -1,20 +1,25 @@
 "use client"
 import { Input } from "@/components/ui/input"
-import { useRef, useState } from "react";
+import { useRef, useState, useEffect } from "react";
 import { getSearchNotes } from "./handleNotes";
 import ShowNotes from "./showNotes";
 
 const SearchComponent = ({ notes }) => {
     const [searchResults, setSearchResults] = useState(notes);
     const inputRef = useRef(null);
+
+    useEffect(() => {
+        setSearchResults(notes);
+    }, [notes]);
+
     const searchNotes = async () => {
         const query = inputRef.current.value;
-        const res = await getSearchNotes(query);
-        if (res) {
-            setSearchResults(res);
-        } else {
-            setSearchResults(searchResults);
+        if (!query) {
+            setSearchResults(notes);
+            return;
         }
+        const res = await getSearchNotes(query);
+        setSearchResults(res);
     }
     return (
         <div>
